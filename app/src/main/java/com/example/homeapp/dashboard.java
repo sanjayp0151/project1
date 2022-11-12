57557ee88627afcb99b33bfc3fc565f39fc5e676
sanjay puri <sanjayp.0151@gmail.com>
2022-11-12 14:19:31 -0800

    Initial commit

---
+++ app/src/main/java/com/example/homeapp/dashboard.java
@@ -0,0 +1,13 @@
+package com.example.homeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
