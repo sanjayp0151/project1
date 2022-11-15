38a8a2e79e312561a00770e725c4f9093089e3b1
aagam <aagamj7229@gmail.com>
2022-11-15 15:27:31 +0530

    xyz

--- app/src/main/java/com/example/homeapp/login.java
+++ app/src/main/java/com/example/homeapp/login.java
@@ -2,16 +2,68 @@ package com.example.homeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class login extends AppCompatActivity {
 
 
+    TextInputLayout username_var, password_var;
+    TextView signup;
+    Button login_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.login);
+
+         signup = findViewById(R.id.for_signup);
+         login_btn = findViewById(R.id.login_button);
+        username_var = findViewById(R.id.login_user_id);
+        password_var = findViewById(R.id.login_password);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = username_var.getEditText().getText().toString();
+                String password = password_var.getEditText().getText().toString();
+
+                if(!username.isEmpty()){
+                    username_var.setError(null);
+                    username_var.setErrorEnabled(false);
+
+                    if(!password.isEmpty()){
+                        password_var.setError(null);
+                        password_var.setErrorEnabled(false);
+                    }else{
+                        password_var.setError("PLEASE ENTER THE PASSWORD");
+                    }
+                }else{
+                    username_var.setError("PLEASE ENTER THE USERNAME");
+                }
+            }
+        });
+
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),signup.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
+
+
 }
--- app/src/main/java/com/example/homeapp/signup.java
+++ app/src/main/java/com/example/homeapp/signup.java
@@ -2,13 +2,83 @@ package com.example.homeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class signup extends AppCompatActivity {
+
+    TextInputLayout fullname_var,username_var,phonenumber_var,password_var,email_var;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.signup);
+
+        fullname_var = findViewById(R.id.user_full_name);
+        username_var = findViewById(R.id.username);
+        phonenumber_var = findViewById(R.id.user_phoneno);
+        password_var = findViewById(R.id.password_input_field);
+        email_var = findViewById(R.id.user_email);
+
+
+
+    }
+
+    public void registerButtonclick(View view) {
+        String fullname = fullname_var.getEditText().getText().toString();
+        String username = username_var.getEditText().getText().toString();
+        String email = email_var.getEditText().getText().toString();
+        String phoneNumber = phonenumber_var.getEditText().getText().toString();
+        String password = password_var.getEditText().getText().toString();
+
+        if(!fullname.isEmpty()){
+            fullname_var.setError(null);
+            fullname_var.setErrorEnabled(false);
+            if(!username.isEmpty()){
+                username_var.setError(null);
+                username_var.setErrorEnabled(false);
+                if(!email.isEmpty()){
+                    email_var.setError(null);
+                    email_var.setErrorEnabled(false);
+                    if(!phoneNumber.isEmpty()){
+                        phonenumber_var.setError(null);
+                        phonenumber_var.setErrorEnabled(false);
+                        if(password.isEmpty()){
+                            password_var.setError(null);
+                            password_var.setErrorEnabled(false);
+                            if(email.matches("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b")){
+
+                            }else{
+                                email_var.setError("Invalid Email");
+                            }
+                        }else{
+                            password_var.setError("PLEASE ENTER THE PASSWORD!");
+                        }
+                    }else{
+                        phonenumber_var.setError("PLEASE ENTER THE PHONE NUMBER");
+                    }
+                }else{
+                    email_var.setError("PLEASE ENTER THE EMAIL");
+                }
+            }else{
+                username_var.setError("PLEASE ENTER THE USERNAME");
+            }
+        }else{
+            fullname_var.setError("PLEASE ENTER THE FULLNAME");
+        }
+
+    }
+
+    public void for_loginclick(View view) {
+        Intent intent = new Intent(getApplicationContext(),login.class);
+        startActivity(intent);
+        finish();
     }
 }
